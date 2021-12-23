4a7d5cd2ca00f964425426e2c6d6637cb223feb5
Dheeraj1122 <dkhandelwal168@gmail.com>
2021-12-23 13:43:40 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,85 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  keyword = 'name';
-  email = 'mail';
-  public countries = [
-    {
-      id: 1,
-      name: 'Albania',
-      mail: 'dkhandelwaldeloitte.com',
-    },
-    {
-      id: 2,
-      name: 'Belgium',
-      mail: 'shahid.com',
-    },
-    {
-      id: 3,
-      name: 'Denmark',
-      mail: 'ravi.com',
-    },
-    {
-      id: 4,
-      name: 'Montenegro',
-      mail: 'ravi.com',
-    },
-    {
-      id: 5,
-      name: 'Turkey',
-      mail: 'test.com',
-    },
-    {
-      id: 6,
-      name: 'Ukraine',
-      mail: 'mehta.com',
-    },
-    {
-      id: 7,
-      name: 'Macedonia',
-      mail: 'ajay.com',
-    },
-    {
-      id: 8,
-      name: 'Slovenia',
-      mail: 'khandelwal.com',
-    },
-    {
-      id: 9,
-      name: 'Georgia',
-      mail: 'kumar.com',
-    },
-    {
-      id: 10,
-      name: 'India',
-      mail: 'acharya.com',
-    },
-    {
-      id: 11,
-      name: 'Russia',
-      mail: 'russ.com',
-    },
-    {
-      id: 12,
-      name: 'Switzerland',
-      mail: 'swiss.com',
-    },
-  ];
-  selectEvent(item) {
-    // do something with selected item
+export class AppComponent implements OnInit {
+  auto: any;
+  keyword = 'test';
+
+  constructor() {
+    // this.data.forEach((obj) => {
+    //   obj.name = obj.name + ' - ' + obj.address;
+    // });
   }
 
-  onChangeSearch(search: string) {
-    // fetch remote data from here
+  ngOnInit() {}
+  Data() {
+    this.auto = [
+      {
+        id: 1,
+        name: 'Virat Sharma',
+        address: 'New York City',
+        test: 'Virat Sharma - New York City',
+      },
+      {
+        id: 2,
+        name: 'Mark Jubin',
+        address: 'Calliforia',
+        test: 'Virat Sharma - New York City',
+      },
+      {
+        id: 3,
+        name: 'Thomas Muller',
+        address: 'Munich',
+        test: 'Virat Sharma - New York City',
+      },
+      {
+        id: 4,
+        name: 'Paul',
+        address: 'Paris',
+        test: 'Virat Sharma - New York City',
+      },
+      {
+        id: 5,
+        name: 'Vein Mark',
+        address: 'England',
+        test: 'Virat Sharma - New York City',
+      },
+      {
+        id: 6,
+        name: 'Robert',
+        address: 'Australia',
+        test: 'Virat Sharma - New York City',
+      },
+      {
+        id: 7,
+        name: 'Tom Smith',
+        address: 'Newzeland',
+        test: 'Virat Sharma - New York City',
+      },
+    ];
+  }
+  selectEvent(item) {
+    alert(item.name);
+    // here we can write code for doing something with selected item
+  }
+
+  onChangeSearch(val: string) {
+    // here we can fetch data from remote location here
     // And reassign the 'data' which is binded to 'data' property.
   }
 
   onFocused(e) {
-    // do something
+    // here we can write our code for doing something when input is focused
   }
 }
